c5f3560c0cb18dd7227405e5f657da3c456454e6
Donna <donnajun1214@gmail.com>
2023-07-12 09:38:57 +0900

    [add] binary files for tflite. Place the script install.sh (Linux/Mac) or install.bat (Windows) at the root of your project. Execute sh install.sh (Linux) / install.bat (Windows) at the root of your project to automatically download and place binaries at appropriate folders. tflite_flutter_helper's build.gradle shoud be modified to 1.6.10

--- lib/logic/tflite_classifier.dart
+++ lib/logic/tflite_classifier.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:image/image.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
