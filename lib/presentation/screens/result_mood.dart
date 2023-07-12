68a75e714cc797993a783f8ae71499831dded201
Donna <donnajun1214@gmail.com>
2023-07-12 14:09:38 +0900

    [modify] app name and package

---
+++ lib/presentation/screens/result_mood.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoodResult extends StatelessWidget {
+  const MoodResult({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(child: Text('MOOD PAGE')),
+    );
+  }
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,3 @@
+export './home.dart';
+export './crop_image.dart';
+export './result_mood.dart';
