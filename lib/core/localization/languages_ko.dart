d2afe99f032162f505037761cb0a1f645629faaa
Donna <donnajun1214@gmail.com>
2023-07-20 17:01:05 +0900

    [fix] typo

--- lib/core/localization/languages_ko.dart
+++ lib/core/localization/languages_ko.dart
@@ -27,7 +27,7 @@ class LanguagesKo extends Languages {
   @override
   String get excitedCat => '흥분한 고양이';
   @override
-  String get failed => '알 수 없는 생물체';
+  String get failed => '알 수 없는 생물체?';
   @override
   String get retry => '다시 시도하기';
   @override
@@ -47,16 +47,16 @@ class LanguagesKo extends Languages {
       '무언가 맘에 안 드는 의뭉스러운 표정같다구요?? 주인님은 현재 마음의 평화를 찾았습니다. 매우 만족스러운 상태입니다.';
   @override
   String get yawnCatContents =>
-      '주인님이 흥미로운 것 또는 낯선 것을 발견하고 매우 흥분한 상태입니다. 혹시 주인님이 엉덩이를 뒤로 쭉 빼고 움찔거리는 상태라면 도망가세요! 맹수가 곧 사냥을 시작합니다!';
+      '그 밖에도 긴장과 불안을 해소하기 위해서도 하품을 합니다. 하지만 과도한 하품은 구내염, 치주염 등 주인님 입안이 불편하다는 사인일 수도 있습니다!';
   @override
   String get angryCatContents =>
-      '그 밖에도 긴장과 불안을 해소하기 위해서도 하품을 합니다. 하지만 과도한 하품은 구내염, 치주염 등 주인님 입안이 불편하다는 사인일 수도 있습니다!';
+      '주인님께서는 매우 심기가 불편합니다. 언제 하악질이 날라와도 이상하지 않은 상황입니다. 조심하세요!';
   @override
   String get sleepyCatContents =>
       '실제로 고양이는 하루 평균 16-20시간을 자는데 쓰는데요. 하지만 이 중에서도 깊은 잠은 겨우 3-4시간 정도에 불과하고 대부분 선잠이라고 합니다. 쿨쿨 주무시는 주인님을 방해하지 않도록 쉿! 모두 로그아웃하세요!';
   @override
   String get excitedCatContents =>
-      '주인님께서는 매우 심기가 불편합니다. 언제 하악질이 날라와도 이상하지 않은 상황입니다. 조심하세요!';
+      '주인님이 흥미로운 것 또는 낯선 것을 발견하고 매우 흥분한 상태입니다. 혹시 주인님이 엉덩이를 뒤로 쭉 빼고 움찔거리는 상태라면 도망가세요! 맹수가 곧 사냥을 시작합니다!';
   @override
   String get failedContents =>
       '어떤 의미로는 굉장히 정상적인 상태입니다... 감히 한낱 인간이 어찌 고양이님의 위대한 기분을 헤아리겠습니까... 하하...\n주인님의 얼굴을 정면에서 잘 찍어서 다시 시도해 주세요:)';
