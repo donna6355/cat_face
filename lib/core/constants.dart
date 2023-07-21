cc00ca0361e66c270e189891df05b219f4f53ffc
Donna <donnajun1214@gmail.com>
2023-07-21 18:01:23 +0900

    [add] loading gif

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -7,6 +7,7 @@ class Constants {
 class Assets {
   Assets._();
   static const String homeImg = 'assets/images/home_img.png';
+  static const String loading = 'assets/images/toss_n_turn.gif';
   static const String tfLabel = 'assets/models/labels.txt';
   static const String tfModel = 'models/model_unquant.tflite';
 }
--- lib/presentation/screens/analyzing.dart
+++ lib/presentation/screens/analyzing.dart
@@ -25,10 +25,7 @@ class Analyzing extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 24),
-              child: Placeholder(
-                fallbackWidth: 320,
-                fallbackHeight: 200,
-              ),
+              child: Image.asset(Assets.loading),
             ),
             if (AdHelper.nativeAd != null)
               ConstrainedBox(
