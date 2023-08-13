d4a32ebc51eedc9e11ebfe4ac4943d71f04a6f3f
Donna <donnajun1214@gmail.com>
2023-08-13 17:13:14 +0900

    [add] safearea

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -8,40 +8,42 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 48),
-          child: Column(
-            children: [
-              OutlinedText(
-                label: Languages.of(context)!.appName,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 24),
-                child: Image.asset(Assets.homeImg, width: 264),
-              ),
-              ParagraphText(Languages.of(context)!.appDescription),
-              ElevatedButton(
-                  onPressed: () =>
-                      ImgHelper.fetchImg(forCam: true, ctx: context),
-                  child: Text(Languages.of(context)!.takePic)),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                  onPressed: () =>
-                      ImgHelper.fetchImg(forCam: false, ctx: context),
-                  style: CommonStyle.reverseBtn,
-                  child: Text(Languages.of(context)!.getPic),
+    return SafeArea(
+      child: Material(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 48),
+            child: Column(
+              children: [
+                OutlinedText(
+                  label: Languages.of(context)!.appName,
                 ),
-              ),
-              UnderlineText(
-                label: Languages.of(context)!.lang,
-                onTap: () =>
-                    changeLanguage(context, Languages.of(context)!.lang),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: Image.asset(Assets.homeImg, width: 264),
+                ),
+                ParagraphText(Languages.of(context)!.appDescription),
+                ElevatedButton(
+                    onPressed: () =>
+                        ImgHelper.fetchImg(forCam: true, ctx: context),
+                    child: Text(Languages.of(context)!.takePic)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () =>
+                        ImgHelper.fetchImg(forCam: false, ctx: context),
+                    style: CommonStyle.reverseBtn,
+                    child: Text(Languages.of(context)!.getPic),
+                  ),
+                ),
+                UnderlineText(
+                  label: Languages.of(context)!.lang,
+                  onTap: () =>
+                      changeLanguage(context, Languages.of(context)!.lang),
+                ),
+              ],
+            ),
           ),
         ),
       ),
